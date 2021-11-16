c3bf60ab36fcdddc0e758e7b22534ace1140be53
skmn3 <sangjin170202@naver.com>
2021-11-16 15:53:36 +0900

    [CREATE] Model
    Board Model 정의하기

---
+++ blog/src/boards/board.model.ts
@@ -0,0 +1,12 @@
+export class Board {
+    id: string;
+    title: string;
+    description: string;
+    status: BoardStatus;
+}
+
+export enum BoardStatus {
+    PUBLIC = 'PUBLIC',
+    PRIVATE = 'PRIVATE'
+
+}
