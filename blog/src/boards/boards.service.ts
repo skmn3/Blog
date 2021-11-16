896764bb3a5a8371db2736b9926a2a97419f7942
skmn3 <sangjin170202@naver.com>
2021-11-16 15:55:00 +0900

    [UPDATE] Service
    Board Model 정의하기

--- blog/src/boards/boards.service.ts
+++ blog/src/boards/boards.service.ts
@@ -1,10 +1,11 @@
 import { Injectable } from '@nestjs/common';
+import { Board } from './board.model';
 
 @Injectable()
 export class BoardsService {
-    private boards = [];
-
-    getAllboards() {
+    private boards: Board[] = [];
+    
+    getAllboards(): Board[] {
         return this.boards;
     }
 }
