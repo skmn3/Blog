42fd1ecaf15184d70de6403a94c92aa3805ea4fa
skmn3 <sangjin170202@naver.com>
2021-11-19 15:04:14 +0900

    [UPDATE] Controller
    게시물 생성을 위한 Dto

--- blog/src/boards/boards.controller.ts
+++ blog/src/boards/boards.controller.ts
@@ -1,6 +1,8 @@
 import { Body, Controller, Get, Post } from '@nestjs/common';
 import { BoardsService } from './boards.service';
-import Board from './board.model';
+import { Board } from './board.model';
+import { CreateBoardDto } from '../boards/dto/create-board.dto';
+
 
 @Controller('boards')
 export class BoardsController {
@@ -13,9 +15,8 @@ export class BoardsController {
 
     @Post()
     createBoard(
-        @Body('title') title: string,
-        @Body('description') description: string
+        @Body() createBoardDto: CreateBoardDto
     ): Board {
-        return this.boardsService.createBoard(title, description);
+        return this.boardsService.createBoard(createBoardDto);
     }
 }
