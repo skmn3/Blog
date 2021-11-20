b8a7bfa118e4221e435bf6ff9cb1c62d2f6956a5
skmn3 <sangjin170202@naver.com>
2021-11-20 19:33:24 +0900

    [UPDATE] Controller
    ID로 특정 게시물 가져오기

--- blog/src/boards/boards.controller.ts
+++ blog/src/boards/boards.controller.ts
@@ -1,8 +1,7 @@
-import { Body, Controller, Get, Post } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post } from '@nestjs/common';
 import { BoardsService } from './boards.service';
 import { Board } from './board.model';
 import { CreateBoardDto } from '../boards/dto/create-board.dto';
-
 
 @Controller('boards')
 export class BoardsController {
@@ -18,5 +17,10 @@ export class BoardsController {
         @Body() createBoardDto: CreateBoardDto
     ): Board {
         return this.boardsService.createBoard(createBoardDto);
+    }
+
+    @Get('/:id')
+    getBoardById(@Param('id') id: string): Board {
+        return this.boardsService.getBoardById(id);
     }
 }
