0fbbc271ad748c752e91266a83892a5ca93ecfa9
skmn3 <sangjin170202@naver.com>
2021-11-22 15:03:27 +0900

    [UPDATE] Controller
    Pipe로 유효성 체크하기

--- blog/src/boards/boards.controller.ts
+++ blog/src/boards/boards.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Delete, Get, Param, Patch, Post } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Patch, Post, UsePipes, ValidationPipe } from '@nestjs/common';
 import { BoardsService } from './boards.service';
 import { Board, BoardStatus } from './board.model';
 import { CreateBoardDto } from '../boards/dto/create-board.dto';
@@ -13,6 +13,7 @@ export class BoardsController {
     }
 
     @Post()
+    @UsePipes(ValidationPipe)
     createBoard(
         @Body() createBoardDto: CreateBoardDto
     ): Board {
