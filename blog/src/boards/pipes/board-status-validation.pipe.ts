2f4d3bbb31c78e3a05b213b1b6b5e753a112a18c
skmn3 <sangjin170202@naver.com>
2021-11-23 15:10:17 +0900

    [CREATE] Pipe
    커스텀 파이프를 이용한 유효성 체크

---
+++ blog/src/boards/pipes/board-status-validation.pipe.ts
@@ -0,0 +1,25 @@
+import { ArgumentMetadata, BadRequestException, ConsoleLogger, PipeTransform } from "@nestjs/common";
+import { BoardStatus } from "../board.model";
+
+export class BoardStatusValidationPipe implements PipeTransform {
+    readonly StatusOptions = [
+        BoardStatus.PRIVATE,
+        BoardStatus.PUBLIC,
+    ]
+    
+    transform(value: any, metadata: ArgumentMetadata){
+        value = value.toUpperCase();
+
+        if(!this.isStatusValid(value)) {
+            throw new BadRequestException(`${value} is not a valid board status`);
+        }
+
+        return value;
+    }
+
+    private isStatusValid(status: any) {
+        const index = this.StatusOptions.indexOf(status);
+
+        return index !== -1;
+    }
+}
