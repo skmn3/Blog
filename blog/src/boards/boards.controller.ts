92d1eea1c9d88dce85de40fb8503e81235fb182a
skmn3 <sangjin170202@naver.com>
2021-11-26 17:39:08 +0900

    [UPDATE] Controller
    데이터베이스 연동을 위한 소스코드 정리

--- blog/src/boards/boards.controller.ts
+++ blog/src/boards/boards.controller.ts
@@ -1,41 +1,8 @@
-import { Body, Controller, Delete, Get, Param, Patch, Post, UsePipes, ValidationPipe } from '@nestjs/common';
+import { Controller } from '@nestjs/common';
 import { BoardsService } from './boards.service';
-import { Board, BoardStatus } from './board.model';
-import { CreateBoardDto } from '../boards/dto/create-board.dto';
-import { BoardStatusValidationPipe } from './pipes/Board-status-validation.pipe';
 
 @Controller('boards')
 export class BoardsController {
     constructor(private boardsService: BoardsService) { }
 
-    // @Get('/')
-    // getAllboard(): Board[] {
-    //     return this.boardsService.getAllboards();
-    // }
-
-    // @Post()
-    // @UsePipes(ValidationPipe)
-    // createBoard(
-    //     @Body() createBoardDto: CreateBoardDto
-    // ): Board {
-    //     return this.boardsService.createBoard(createBoardDto);
-    // }
-
-    // @Get('/:id')
-    // getBoardById(@Param('id') id: string): Board {
-    //     return this.boardsService.getBoardById(id);
-    // }
-
-    // @Delete('/:id')
-    // deleteBoard(@Param('id') id: string): void {
-    //     this.boardsService.deleteBoard(id);
-    // }
-
-    // @Patch('/:id/status')
-    // updateBoardStatus(
-    //     @Param('id') id: string, 
-    //     @Body('status', BoardStatusValidationPipe) status: BoardStatus,
-    // ) {
-    //     return this.boardsService.updateBoardStatus(id, status);
-    // }
 }
