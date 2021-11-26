0efe2de13ce4dda9b48e30b87dfaaa9767a6425e
skmn3 <sangjin170202@naver.com>
2021-11-26 17:39:35 +0900

    [UPDATE] Service
    데이터베이스 연동을 위한 소스코드 정리

--- blog/src/boards/boards.service.ts
+++ blog/src/boards/boards.service.ts
@@ -1,51 +1,7 @@
-import { Injectable, NotFoundException } from '@nestjs/common';
-import { Board, BoardStatus } from './board.model';
-import { v4 as uuid } from 'uuid';
-import { CreateBoardDto } from '../boards/dto/create-board.dto';
+import { Injectable } from '@nestjs/common';
+
 @Injectable()
 export class BoardsService {
-    getBoard(): Board {
-        throw new Error('Method not implemented.');
-    }
-    private boards: Board[] = [];
+
     
-    getAllboards(): Board[] {
-        return this.boards;
-    }
-
-    createBoard(createBoardDto: CreateBoardDto): Board {
-        const { title, description } = createBoardDto;
-        const board: Board = {
-            id: uuid(),
-            title,
-            description,
-            status: BoardStatus.PUBLIC,
-        }
-
-        this.boards.push(board);
-        return board;
-    }
-
-    getBoardById(id: string): Board {
-        const found = this.boards.find((board) => board.id === id);
-
-        if(!found) {
-            throw new NotFoundException(`Can't find board with id ${id}`);
-        }
-        
-        return found;
-
-    }
-
-    deleteBoard(id: string): void {
-        const found = this.getBoardById(id);
-
-        this.boards = this.boards.filter((board) => board.id !== found.id);
-    }
-
-    updateBoardStatus(id: string, status: BoardStatus): Board {
-        const board = this.getBoardById(id);
-        board.status = status;
-        return board;
-    }
 }
