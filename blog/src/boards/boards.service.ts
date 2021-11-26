2dda5cfb017f692fca6cfc133b49669c6abbe5c4
skmn3 <sangjin170202@naver.com>
2021-11-26 17:53:17 +0900

    [UPDATE] Service
    아이디를 이용해 특정 게시물 가져오기

--- blog/src/boards/boards.service.ts
+++ blog/src/boards/boards.service.ts
@@ -1,7 +1,23 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Board } from './board.entity';
+import { BoardRepository } from './board.repository';
 
 @Injectable()
 export class BoardsService {
+    constructor(
+        @InjectRepository(BoardRepository)
+        private boardRepository: BoardRepository,
 
-    
+    ){}
+
+    async getBoardById(id: number): Promise<Board> {
+        const found = await this.boardRepository.findOne(id);
+
+        if(!found) {
+            throw new NotFoundException(`Can't find board ${id}`);
+        }
+
+        return found;
+    }
 }
