095506852964b1ad44cb1cdf18fade7f274d14b3
skmn3 <sangjin170202@naver.com>
2021-11-26 17:38:56 +0900

    [UPDATE] Repository
    데이터베이스 연동을 위한 소스코드 정리

--- blog/src/boards/board.repository.ts
+++ blog/src/boards/board.repository.ts
@@ -1,5 +1,5 @@
 import { EntityRepository, Repository } from "typeorm";
-import { Board } from "./board.model";
+import { Board } from "./board.entity";
 
 @EntityRepository(Board)
 export class BoardRepository extends Repository<Board> {
