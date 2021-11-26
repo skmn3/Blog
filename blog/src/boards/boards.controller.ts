cea5860eb6df683707302a10f902eefa360abeef
skmn3 <sangjin170202@naver.com>
2021-11-26 17:54:04 +0900

    [UPDATE] Controller
    아이디를 이용해 특정 게시물 가져오기

--- blog/src/boards/boards.controller.ts
+++ blog/src/boards/boards.controller.ts
@@ -1,8 +1,13 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
+import { Board } from './board.entity';
 import { BoardsService } from './boards.service';
 
 @Controller('boards')
 export class BoardsController {
     constructor(private boardsService: BoardsService) { }
-
+    
+    @Get('/:id')
+    getBoardById(@Param('id') id: number): Promise<Board> {
+        return this.boardsService.getBoardById(id);
+    }
 }
