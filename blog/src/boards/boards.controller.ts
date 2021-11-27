855586ef26222507b8c092ed27a5c76f76b97d45
skmn3 <sangjin170202@naver.com>
2021-11-27 15:52:38 +0900

    [UPDATE] Controller
    게시물 생성하기

--- blog/src/boards/boards.controller.ts
+++ blog/src/boards/boards.controller.ts
@@ -1,6 +1,7 @@
-import { Controller, Get, Param } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post, UsePipes, ValidationPipe } from '@nestjs/common';
 import { Board } from './board.entity';
 import { BoardsService } from './boards.service';
+import { CreateBoardDto } from './dto/create-board.dto';
 
 @Controller('boards')
 export class BoardsController {
@@ -9,5 +10,11 @@ export class BoardsController {
     @Get('/:id')
     getBoardById(@Param('id') id: number): Promise<Board> {
         return this.boardsService.getBoardById(id);
+    }
+
+    @Post()
+    @UsePipes(ValidationPipe)
+    createBoard(@Body() createBoardDto: CreateBoardDto): Promise<Board> {
+        return this.boardsService.createBoard(createBoardDto);
     }
 }
