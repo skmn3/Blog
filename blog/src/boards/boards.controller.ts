0f26b8d32b2ed1dea4805cc1c517ad7764a9b35f
skmn3 <sangjin170202@naver.com>
2021-11-27 16:22:00 +0900

    [UPDATE] Controller
    모든 게시물 가져오기

--- blog/src/boards/boards.controller.ts
+++ blog/src/boards/boards.controller.ts
@@ -9,6 +9,11 @@ import { BoardStatusValidationPipe } from './pipes/board-status-validation.pipe'
 export class BoardsController {
     constructor(private boardsService: BoardsService) { }
     
+    @Get()
+    getAllBoard(): Promise<Board[]> {
+        return this.boardsService.getAllBoards();
+    }
+
     @Get('/:id')
     getBoardById(@Param('id') id: number): Promise<Board> {
         return this.boardsService.getBoardById(id);
