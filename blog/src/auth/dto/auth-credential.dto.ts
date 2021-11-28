d7cf59bca382cd0f820b0281a746b9e58457f080
skmn3 <sangjin170202@naver.com>
2021-11-28 13:56:37 +0900

    [UPDATE] Dto
    User 데이터 유효성 체크

--- blog/src/auth/dto/auth-credential.dto.ts
+++ blog/src/auth/dto/auth-credential.dto.ts
@@ -1,5 +1,22 @@
+import { IsOptional, IsString, Matches, MaxLength, MinLength } from "class-validator";
+
 export class AuthCredentialsDto {
+    @IsString()
+    @MinLength(4)
+    @MaxLength(20)
     username: string;
+    
+    @IsString()
+    @MinLength(6)
+    @MaxLength(64)
+    @Matches(/^[a-zA-Z0-9@#]*$/, {
+        message: 'Only English letters and numbers, and @ and # are allowed for passwords.'
+    })
     password: string;
+
+    @IsString()
+    @MinLength(10)
+    @MaxLength(36)
     email: string;
 }
+
