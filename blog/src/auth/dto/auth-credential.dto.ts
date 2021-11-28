afb124c1898ca0b4c0385811c41b09e5dcb49a7d
skmn3 <sangjin170202@naver.com>
2021-11-28 13:57:56 +0900

    [FIX] Dto
    import 잘못 된 거 삭제

--- blog/src/auth/dto/auth-credential.dto.ts
+++ blog/src/auth/dto/auth-credential.dto.ts
@@ -1,4 +1,4 @@
-import { IsOptional, IsString, Matches, MaxLength, MinLength } from "class-validator";
+import { IsString, Matches, MaxLength, MinLength } from "class-validator";
 
 export class AuthCredentialsDto {
     @IsString()
