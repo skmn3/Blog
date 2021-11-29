1c02b8a7bae78e43de55cfe0af102aaf8b703cac
skmn3 <sangjin170202@naver.com>
2021-11-29 15:27:58 +0900

    [UPDATE] Controller
    JWT 토큰 발급

--- blog/src/auth/auth.controller.ts
+++ blog/src/auth/auth.controller.ts
@@ -14,7 +14,7 @@ export class AuthController {
     }
 
     @Post('/signin')
-    signIn(@Body(ValidationPipe) authCredentialsDto: AuthCredentialsDto) {
+    signIn(@Body(ValidationPipe) authCredentialsDto: AuthCredentialsDto): Promise<{accessToken}>{
         return this.authService.signIn(authCredentialsDto);
     }
 }
