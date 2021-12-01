9487b0570d3a7b03a677bd25f478acdb4834cf29
skmn3 <sangjin170202@naver.com>
2021-12-01 16:10:34 +0900

    [UPDATE] Main
    로그

--- blog-project/src/main.ts
+++ blog-project/src/main.ts
@@ -1,8 +1,13 @@
 import { NestFactory } from '@nestjs/core';
+import { Logger } from '@nestjs/common'
 import { AppModule } from './app.module';
 
 async function bootstrap() {
+  const logger = new Logger();
   const app = await NestFactory.create(AppModule);
-  await app.listen(4000);
+  
+  const PORT = 4000;
+  await app.listen(PORT);
+  logger.log(`Aplication running on PORT: ${PORT}`);
 }
 bootstrap();
