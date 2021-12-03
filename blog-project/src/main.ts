262a173cfea7c28207ae2e0cdd72cbfe08605d85
skmn3 <sangjin170202@naver.com>
2021-12-03 15:57:23 +0900

    [UPDATE] Main
    Config 설정 적용

--- blog-project/src/main.ts
+++ blog-project/src/main.ts
@@ -1,12 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { Logger } from '@nestjs/common'
 import { AppModule } from './app.module';
+import * as config from 'config';
 
 async function bootstrap() {
   const logger = new Logger();
   const app = await NestFactory.create(AppModule);
   
-  const PORT = 4000;
+  const ServerConfig = config.get('server');
+  const PORT = ServerConfig.port;
   await app.listen(PORT);
   logger.log(`Aplication running on PORT: ${PORT}`);
 }
