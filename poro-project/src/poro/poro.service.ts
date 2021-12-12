f85e194fc019d8018db6890b36d4a11b50a1b59e
skmn3 <sangjin170202@naver.com>
2021-12-12 14:08:59 +0900

    [FIX] Service
    파일 이름 변경

--- poro-project/src/poro/poro.service.ts
+++ poro-project/src/poro/poro.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
-import { PoroRepository } from './poro.repositiry';
+import { PoroRepository } from './poro.repository';
 
 @Injectable()
 export class PoroService {
